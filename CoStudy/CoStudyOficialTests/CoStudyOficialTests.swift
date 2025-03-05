5d1b31c825f56f03b20525ecb08859cc93aa1277
Matheus <41300255+matheusct27@users.noreply.github.com>
2025-03-05 08:06:50 -0300

    Create CoStudyOficialTests.swift

---
+++ CoStudy/CoStudyOficialTests/CoStudyOficialTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoStudyOficialTests.swift
+//  CoStudyOficialTests
+//
+//  Created by found on 11/02/25.
+//
+
+import Testing
+@testable import CoStudyOficial
+
+struct CoStudyOficialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

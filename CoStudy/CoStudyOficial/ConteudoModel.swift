7b9756fe5c2b18fa8513542078037c1767916531
Matheus <41300255+matheusct27@users.noreply.github.com>
2025-03-05 08:00:46 -0300

    Create ConteudoModel.swift

---
+++ CoStudy/CoStudyOficial/ConteudoModel.swift
@@ -0,0 +1,13 @@
+//
+//  ConteudoModel.swift
+//  CoStudyOficial
+//
+//  Created by found on 25/02/25.
+//
+import SwiftUI
+
+struct Conteudo : Identifiable {
+    let id = UUID()
+    var nome : String
+    var paginaDeTexto : String
+}

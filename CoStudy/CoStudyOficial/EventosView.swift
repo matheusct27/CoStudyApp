c86034aa9348d7796f7d557e0637dd1d3521717f
Matheus <41300255+matheusct27@users.noreply.github.com>
2025-03-05 08:01:47 -0300

    Create EventosView.swift

---
+++ CoStudy/CoStudyOficial/EventosView.swift
@@ -0,0 +1,18 @@
+//
+//  EventosView.swift
+//  CoStudyOficial
+//
+//  Created by found on 11/02/25.
+//
+import SwiftUI
+
+struct EventosView: View {
+    var body: some View {
+        ZStack {
+            Color(hex: "FFFBEF")
+                .ignoresSafeArea(.all)
+                .allowsHitTesting(false)
+            Text("Tela de Eventos")
+        }
+    }
+}

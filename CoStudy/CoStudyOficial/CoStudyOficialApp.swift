7a953809cee8a7c96651647fae3cf14af86db6ac
Matheus <41300255+matheusct27@users.noreply.github.com>
2025-03-05 07:52:15 -0300

    Create CoStudyOficialApp.swift

---
+++ CoStudy/CoStudyOficial/CoStudyOficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoStudyOficialApp.swift
+//  CoStudyOficial
+//
+//  Created by found on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CoStudyOficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
